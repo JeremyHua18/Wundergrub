20e2ca72bedf193a12e61d981267f38dfbc4a8c9
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-09-03 01:21:34 -0400

    email validation

--- test/src/components/Register.js
+++ test/src/components/Register.js
@@ -5,6 +5,11 @@ class Register extends Component {
 	constructor(props) {
 		super(props);
 
+		this.okNAme = false;
+		this.okEmail = false;
+		this.okPass = false;
+		this.okComfirm = false;
+
 		this.state = {
 			fullname: '',
 			email: '',
@@ -23,8 +28,10 @@ class Register extends Component {
 			let password = this.state.password;
 			if (value !== password) {
 				document.getElementById("unconfirmedPassword").innerHTML = "Passwords do not match!";
+				this.okComfirm = false;
 			} else {
 				document.getElementById("unconfirmedPassword").innerHTML = "";
+				this.okComfirm = true;
 			}
 		} else {
 			if (name === "password") {
@@ -37,30 +44,44 @@ class Register extends Component {
 				if (!validator1.validate(value)) {
 					document.getElementById("invalidPassword").innerHTML =
 						"Password length must be greater than 6 and less than 50.";
+					this.okPass = false;
 				} else if (!validator2.validate(value)) {
 					document.getElementById("invalidPassword").innerHTML =
 						"Password must contain a lowercase letter.";
+					this.okPass = false;
 				} else if (!validator3.validate(value)) {
 					document.getElementById("invalidPassword").innerHTML =
 						"Password must contain an uppercase letter.";
+					this.okPass = false;
 				} else if (!validator4.validate(value)) {
 					document.getElementById("invalidPassword").innerHTML =
 						"Password must contain a number.";
+					this.okPass = false;
 				} else if (!validator5.validate(value)) {
 					document.getElementById("invalidPassword").innerHTML =
 						"Password must not contain space.";
+					this.okPass = false;
 				} else {
 					document.getElementById("invalidPassword").innerHTML = "";
+					this.okPass = true;
 				}
 				this.setState({
 					[name]: value
 				});
-			} else {
-				this.setState({
-					[name]: value
-				});
+			} else if (name === "email") {
+				const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i
+				if(! expression.test(String(value).toLowerCase())) {
+					document.getElementById("invalidEmail").innerHTML =
+						"Your E-mail address is not valid.";
+					this.okPass = false;
+				} else {
+					document.getElementById("invalidEmail").innerHTML = "";
+					this.okPass = true;
+				}
 			}
-
+			this.setState({
+				[name]: value
+			});
 		}
 	}
 
@@ -100,6 +121,7 @@ class Register extends Component {
 							value={this.state.email}
 							onChange={this.update}
 						/>
+						<div id="invalidEmail" className="registrationError"></div>
 					</div>
 
 					<div className="pasword">
@@ -110,7 +132,7 @@ class Register extends Component {
 							value={this.state.password}
 							onChange={this.update}
 						/>
-						<div id="invalidPassword"></div>
+						<div id="invalidPassword" className="registrationError"></div>
 					</div>
 
 					<div className="password">
@@ -120,7 +142,7 @@ class Register extends Component {
 							name="password1"
 							onChange={this.update}
 						/>
-						<div id="unconfirmedPassword"></div>
+						<div id="unconfirmedPassword" className="registrationError"></div>
 					</div>
 
 					<input type="submit" value="submit" />
