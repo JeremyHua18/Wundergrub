92648391e3baa63b97560980eb4530017c8a13db
tzuo9 <70283133+tzuo9@users.noreply.github.com>
2020-09-14 21:53:38 +0800

    Update manage_account.js

--- test/src/components/manage_account.js
+++ test/src/components/manage_account.js
@@ -4,15 +4,23 @@ import { Link } from 'react-router-dom';
 
 class manage_account extends Component {
 	render() {
+		const { user} = this.props;
 		return (
 			<div className="account">
                 <h5>Home->manage account</h5>
                 <h2>Manage Account</h2>
-				<h4>to implement later</h4>
+				<h4>Current Account: {localStorage.getItem('userEmail')} </h4>				
+				<button onClick = {deleteAccount} >Delete My Account</button>
 				<Link className="link"to="/home">return home</Link>
 			</div>
 		);
 	}
+}
+
+function deleteAccount() {
+	localStorage.clear();
+	//console.log('account has been deleted');
+	window.location.reload(false);
 }
 
 export default manage_account;
