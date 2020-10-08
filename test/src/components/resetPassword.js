854d7dfae786f5915fe843bb894a8b299e7f4381
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-10-08 17:22:24 -0400

    send email & resend email

--- test/src/components/resetPassword.js
+++ test/src/components/resetPassword.js
@@ -2,6 +2,7 @@ import {Link, withRouter} from "react-router-dom";
 import React, { Component } from 'react';
 import UserDataService from "../services/user.service";
 import Cookies from 'universal-cookie';
+import WonderEmail from "../emailer/WonderEmail"
 
 class ResetPassword extends Component {
     constructor(props) {
@@ -10,17 +11,18 @@ class ResetPassword extends Component {
         this.okEmail = false;
         this.okPass = false;
         this.okComfirm = false;
+        this.countDown = -1;
 
         this.state = {
             email: '',
             password: '',
             conf_password: '',
             gen_code: '',
-            input_code: '',
-            countdown: 0
+            input_code: ''
         };
 
-        //this.update = this.update.bind(this);
+        this.update = this.update.bind(this);
+        this.sendEmail = this.sendEmail.bind(this);
     }
 
 
@@ -41,7 +43,7 @@ class ResetPassword extends Component {
                             type="text"
                             placeholder="Your Email"
                             value={this.state.email}
-                            onChange={this.update}
+                            onChange={this.update.bind(this)}
                             name="email"
                         />
                         <div id="invalidEmail" className="registrationError"></div>
@@ -50,7 +52,7 @@ class ResetPassword extends Component {
 
                     <div className="rp-request-resend" style={{display: "none"}}>
                         <p>Have not got the email? Request another email:
-                            <button onClick = {this.resendEmail.bind(this) } className="2nd-send-btn"> Re-send </button>
+                            <button onClick = {this.resendEmail.bind(this)}  id="resend-btn" className="2nd-send-btn"> Re-send </button>
                         </p>
                     </div>
 
@@ -59,7 +61,7 @@ class ResetPassword extends Component {
                             type="text"
                             placeholder="Code"
                             value={this.state.input_code}
-                            onChange={this.update}
+                            onChange={this.update.bind(this)}
                             name="input_code"
                         />
                         <div id="invalidCode" className="registrationError"></div>
@@ -75,7 +77,7 @@ class ResetPassword extends Component {
                             placeholder="Password"
                             name="password"
                             value={this.state.password}
-                            onChange={this.update}
+                            onChange={this.update.bind(this)}
                         />
                         <div id="invalidPassword" className="registrationError"></div>
                     </div>
@@ -86,7 +88,7 @@ class ResetPassword extends Component {
                             placeholder="Confirm Password"
                             name="conf_password"
                             value={this.state.conf_password}
-                            onChange={this.update}
+                            onChange={this.update.bind(this)}
                         />
                         <div id="unconfirmedPassword" className="registrationError"></div>
                     </div>
@@ -153,6 +155,9 @@ class ResetPassword extends Component {
                 this.okComfirm = true;
             }
         }
+        this.setState({
+            [name]: value
+        });
     }
 
     sendEmail(e) {
@@ -160,26 +165,81 @@ class ResetPassword extends Component {
         if (!this.okEmail) {
             alert("Invalid E-mail address!");
         } else {
-            var row = UserDataService.get(this.state.email);
+            document.getElementsByClassName("rp-request-resend")[0].style.display = "block";
+            document.getElementsByClassName("rp-enter-code")[0].style.display = "block";
+            document.getElementsByClassName("rp-enter-email")[0].style.display = "none";
             var self = this;
-            row.then(function (result) {
-                if (result.data === '') {
-                    alert("This e-mail address is not used!");
-                } else {
-                    var nodemailer = require('nodemailer');
+            var x = setInterval(function() {
+                var btn = document.getElementById("resend-btn");
+                var con1 = false;
+                var con2 = false;
+                con1 = document.getElementsByClassName("rp-check-identification")[0].style.display !== 'none' &&
+                    document.getElementsByClassName("rp-request-resend")[0].style.display !== 'none';
 
+                con2 = btn.innerHTML !== 'Re-send';
+
+                if (con1 && con2 && self.countDown >= 0) {
+                    self.countDown -= 1;
+                    btn.innerHTML = self.countDown
                 }
-            });
+                if (self.countDown <= 0) {
+                    btn.innerHTML = 'Re-send'
+                    self.countDown = 60;
+                }
+            }, 1000);
+            // var row = UserDataService.get(this.state.email);
+            // var self = this;
+            // row.then(function (result) {
+            //     if (result.data === '') {
+            //         alert("This e-mail address is not used!");
+            //     } else {
+            //         var data = WonderEmail.sendResetPasswrodCode(self.state.email);
+            //         self.state.gen_code = data.hashCode;
+            //         document.getElementsByClassName("rp-request-resend")[0].style.display = "block";
+            //         document.getElementsByClassName("rp-enter-email")[0].style.display = "none";
+            //         document.getElementsByClassName("rp-enter-code")[0].style.display = "block";
+            //     }
+            // });
         }
 
     }
 
     resendEmail(e) {
+        var btn = document.getElementById("resend-btn");
+        console.log(this.countDown);
+        if (btn.innerHTML === 'Re-send') {
+            this.countDown = 60;
+            btn.innerHTML = this.countDown;
+        }
 
+        // var row = UserDataService.get(this.state.email);
+        // var self = this;
+        // row.then(function (result) {
+        //     if (result.data === '') {
+        //         alert("This e-mail address is not used!");
+        //     } else {
+        //         var data = WonderEmail.sendResetPasswrodCode(self.state.email);
+        //         self.state.gen_code = data.hashCode;
+        //     }
+        // });
     }
 
     checkCode(e) {
+        console.log(this.state.input_code);
+        var hashed = this.state.gen_code;
+        console.log(hashed);
+        var passwordHash = require('password-hash');
 
+        // Important: Delete this block later
+        hashed = passwordHash.generate(this.state.input_code);
+        // Important
+
+        if(passwordHash.verify(this.state.input_code, hashed)) {
+            document.getElementsByClassName("rp-check-identification")[0].style.display = "none";
+            document.getElementsByClassName("rp-reset-password")[0].style.display = "block";
+        } else {
+            alert("Your code is incorrect. If you requested the email multiple times, please check the newest one.");
+        }
     }
 
     changePassword(e) {
