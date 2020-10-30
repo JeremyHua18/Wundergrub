3995079cb16e75aad9f0d0830ba13d800d32d117
tzuo9 <70283133+tzuo9@users.noreply.github.com>
2020-10-31 00:30:23 +0800

    update help

--- test/src/components/help.js
+++ test/src/components/help.js
@@ -3,18 +3,59 @@ import { Link } from 'react-router-dom';
 
 
 class help extends Component {
-	render() {
+	constructor(props) {
+		super(props);
+		this.state = {
+		  value: 'Please leave your question here and our team will contact you later via email.'
+		};
+	
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.textAreaRef = React.createRef();
+
+	  }
+	
+	  handleChange(event) {
+		this.setState({value: event.target.value});
+	  }
+	
+	  handleSubmit(event) {
+		alert('Your question was submitted: ' + this.state.value);
+		event.preventDefault();
+	  }
+
+    componentDidMount() {
+        this.textareaChange(this.textAreaRef.current);
+    }
+
+    textareaChange(ta) {
+        ta.style.height = "100px";
+        ta.style.height = ta.scrollHeight + "px";
+    }
+	
+	  render() {
 		return (
 			<div className="help">
-                <h5><Link className = "link" to="/home">Home</Link></h5>
-                <h2>Help Center</h2>
-				<h4>to implement later</h4>
-				<Link className="link"to="/home">return home</Link>
+				<h5><Link className = "link" to="/home">Home</Link></h5>
+				<h2>Help Center</h2>
+				<form onSubmit={this.handleSubmit}>
+					<label>
+					<br styles="clear:both" />
+                	<div className="form-group">
+                  		<input type="text" className="form-control" name="title" placeholder="Question Title" required />
+                	</div>
+					<textarea ref={this.textAreaRef} style = {{width: 400}} value={this.state.value} onChange={this.handleChange}/>
+					</label>
+					<input type="submit" value="Submit" />
+				</form>
 				<div class="image"></div>
+		  </div>
 
-			</div>
 		);
-	}
+	  }
+
+
 }
 
 export default help;
+
