e2d88436c62f2d72b4baa8c921d284693b67df72
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-11-18 22:24:55 -0500

    add files

---
+++ nodejs-express-sequelize-mysql/app/controllers/s3.controller.js
@@ -0,0 +1,4 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const bucketName = 'your.bucket.name';
+const key = 'data/data.json';
---
+++ test/src/s3/s3.service.js
@@ -0,0 +1,7 @@
+import http from "../http-common";
+
+class WunderS3 {
+
+}
+
+export default new WunderS3();
